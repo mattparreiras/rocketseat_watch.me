5c1be0b06e0e4a8b7f9c0d34b93f863dfa513840
mpsantos8 <matheus.masan@gmail.com>
2021-04-10 16:14:55 -0300

    Delete the MovieProps from the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,12 @@ import { api } from './services/api';
 import './styles/global.scss';
 
 import './styles/sidebar.scss';
-import './styles/content.scss';
+
 
 interface GenreResponseProps {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
   title: string;
-}
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
 }
 
 export function App() {
